600203f5c8d9128b3192250e6dced05165bd29f4
Shushanna Manukyan <manukyanshushanna21@gmail.com>
2026-03-04 23:52:39 +0400

    Add RAM monitoring

--- Monitor.sh
+++ Monitor.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-SYSTEM_STATUS="System Check:"
+SYSTEM_STATUS="System Check: RAM Available is $(free -m | awk '/Mem:/ {print $7 " MB"}') | Disk Available is $(df / | awk 'NR==2 {print 100 - $5 "%"}')"
